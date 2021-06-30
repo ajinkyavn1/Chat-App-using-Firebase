8bd7830e8fa110c6fe4b75ec599814511944ef19
ajinkyavn1 <ajinkyanarkhede7@gmail.com>
2021-06-30 21:19:17 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Chatt-Application-Using Firebase
+ in this application we create a chat application using firebase
---
+++ android/app/src/main/kotlin/com/chatApp/chatapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.chatApp.chatapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
